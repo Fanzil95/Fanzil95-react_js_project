6f2d320acd77699a944ff3da9596d4c75eb57134
Fanzil95 <amirov_fanzil@mail.ru>
2022-01-11 21:41:44 +0500

    adding filtering

--- src/api/fake.api/user.api.js
+++ src/api/fake.api/user.api.js
@@ -1,44 +1,12 @@
-const professions = {
-    doctor: { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" },
-    waiter: { _id: "67rdca3eeb7f6fgeed471820", name: "Официант" },
-    physics: { _id: "67rdca3eeb7f6fgeed471814", name: "Физик" },
-    engineer: { _id: "67rdca3eeb7f6fgeed471822", name: "Инженер" },
-    actor: { _id: "67rdca3eeb7f6fgeed471824", name: "Актер" },
-    cook: { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" }
-};
+import { professions } from "./professions.api";
 const qualities = {
-    tedious: {
-        _id: "67rdca3eeb7f6fgeed471198",
-        name: "Нудила",
-        color: "primary"
-    },
-    strange: {
-        _id: "67rdca3eeb7f6fgeed471100",
-        name: "Странный",
-        color: "secondary"
-    },
-    buller: {
-        _id: "67rdca3eeb7f6fgeed4711012",
-        name: "Троль",
-        color: "success"
-    },
-    alcoholic: {
-        _id: "67rdca3eeb7f6fgeed471101",
-        name: "Алкоголик",
-        color: "danger"
-    },
-    handsome: {
-        _id: "67rdca3eeb7f6fgeed471102",
-        name: "Красавчик",
-        color: "info"
-    },
-    uncertain: {
-        _id: "67rdca3eeb7f6fgeed471103",
-        name: "Неуверенный",
-        color: "dark"
-    }
+    tedious: { _id: "67rdca3eeb7f6fgeed471198", name: "Нудила", color: "primary" },
+    strange: { _id: "67rdca3eeb7f6fgeed471100", name: "Странный", color: "secondary" },
+    buller: { _id: "67rdca3eeb7f6fgeed4711012", name: "Троль", color: "success" },
+    alcoholic: { _id: "67rdca3eeb7f6fgeed471101", name: "Алкоголик", color: "danger" },
+    handsome: { _id: "67rdca3eeb7f6fgeed471102", name: "Красавчик", color: "info" },
+    uncertain: { _id: "67rdca3eeb7f6fgeed471103", name: "Неуверенный", color: "dark" }
 };
-
 const users = [
     {
         _id: "67rdca3eeb7f6fgeed471815",
@@ -46,8 +14,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.tedious, qualities.uncertain, qualities.strange],
         completedMeetings: 36,
-        rate: 2.5,
-        bookmark: false
+        rate: 2.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471816",
@@ -55,8 +22,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.buller, qualities.handsome, qualities.alcoholic],
         completedMeetings: 15,
-        rate: 2.5,
-        bookmark: false
+        rate: 2.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471817",
@@ -64,8 +30,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.buller],
         completedMeetings: 247,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471818",
@@ -73,8 +38,7 @@ const users = [
         profession: professions.waiter,
         qualities: [qualities.uncertain],
         completedMeetings: 148,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471819",
@@ -82,8 +46,7 @@ const users = [
         profession: professions.physics,
         qualities: [qualities.strange, qualities.tedious],
         completedMeetings: 37,
-        rate: 4.6,
-        bookmark: false
+        rate: 4.6
     },
     {
         _id: "67rdca3eeb7f6fgeed471820",
@@ -91,8 +54,7 @@ const users = [
         profession: professions.physics,
         qualities: [qualities.strange, qualities.uncertain],
         completedMeetings: 147,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471821",
@@ -100,8 +62,7 @@ const users = [
         profession: professions.engineer,
         qualities: [qualities.strange, qualities.tedious],
         completedMeetings: 72,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471822",
@@ -109,8 +70,7 @@ const users = [
         profession: professions.engineer,
         qualities: [qualities.handsome],
         completedMeetings: 72,
-        rate: 5,
-        bookmark: false
+        rate: 5
     },
     {
         _id: "67rdca3eeb7f6fgeed471823",
@@ -118,8 +78,7 @@ const users = [
         profession: professions.cook,
         qualities: [qualities.strange, qualities.uncertain],
         completedMeetings: 17,
-        rate: 4.5,
-        bookmark: false
+        rate: 4.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471824",
@@ -127,8 +86,7 @@ const users = [
         profession: professions.cook,
         qualities: [qualities.handsome, qualities.buller],
         completedMeetings: 17,
-        rate: 4.5,
-        bookmark: false
+        rate: 4.5
     },
     {
         _id: "67rdca3eeb7f6fgeed47181f",
@@ -136,8 +94,7 @@ const users = [
         profession: professions.actor,
         qualities: [qualities.uncertain, qualities.strange],
         completedMeetings: 434,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed47181r",
@@ -145,8 +102,7 @@ const users = [
         profession: professions.actor,
         qualities: [qualities.handsome],
         completedMeetings: 434,
-        rate: 5,
-        bookmark: false
+        rate: 5
     }
 ];
 export function fetchAll() {
